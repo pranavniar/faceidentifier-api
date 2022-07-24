5b5c5080efe4b9059dcdeed9920480ec40482a2f
pranavniar <pranavnairdemon@gmail.com>
2022-07-24 07:51:50 +0530

    adding sarting project

---
+++ controllers/signin.js
@@ -0,0 +1,27 @@
+const handleSignin = (req, res, db, bcrypt)=>{
+
+
+    db.select('email','hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if(isValid){
+        return db.select('*').from('users')
+        .where('email', '=', req.body.email)
+        .then(user=>{
+
+          res.json(user[0])
+        })
+        .catch(err=>res.status(400).json('unable to fetch user'))
+      }
+      else{
+        res.status(400).json('invalid password')
+      }
+
+    })
+    .catch(err=>res.status(401).json('invalid password'))
+
+}
+module.exports = {
+  handleSignin: handleSignin
+};
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const app = express();
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    port : 5432,
+    user : 'postgres',
+    password : 'test',
+    database : 'smartbrain'
+  }
+});
+
+
+
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // "*" means allow connection from any request url.
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+app.post('/signin', (req,res)=>signin.handleSignin(req, res, db, bcrypt));
+
+app.post('/register',(req,res)=>register.handleRegister(req, res, db, bcrypt));
+
+app.get('/profile/:id',(req,res)=>profile.handleProfile(req, res, db));
+
+app.put('/image',(req,res)=>image.getImage(req, res, db) )
+
+app.listen(3000, ()=>{
+
+  console.log('app running');
+})
