f884bb8e60aa1a2c3e69a8fcbd651c11a07c0d2e
pranavniar <pranavnairdemon@gmail.com>
2022-07-24 11:43:29 +0530

    psq upgrade

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    port : 5432,
+    port : 5433,
     user : 'postgres',
     password : 'test',
     database : 'smartbrain'
