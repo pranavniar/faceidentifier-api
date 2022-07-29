441e9d839c062dc62cbb19422c6d98a656752f9f
pranavniar <pranavnairdemon@gmail.com>
2022-07-29 15:44:03 +0530

    changes

--- server.js
+++ server.js
@@ -13,11 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    port : process.env.PORT,
-    user : 'postgres',
-    password : 'test',
-    database : 'smartbrain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
